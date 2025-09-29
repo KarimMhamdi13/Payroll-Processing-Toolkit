03d97bd677b10525f9bee88a8602fa9ec5e98df9
Karim Mhamdi <karimmhamdi@usf.edu>
2025-09-28 23:16:36 -0400

    Step 2 done

--- cc_5.js
+++ cc_5.js
@@ -1,2 +1,10 @@
 // Coding Challenge 05
 
+// Step 2
+const employees = [
+  { name: "John Smith", hourlyRate: 29.99, hoursWorked: 8 },
+  { name: "Robert Dwoney Jr.", hourlyRate: 99.50,         hoursWorked: 10  },
+  { name: "Amuro Ray", hourlyRate: 49.99, hoursWorked: 6  },
+];
+
+// Step 3
