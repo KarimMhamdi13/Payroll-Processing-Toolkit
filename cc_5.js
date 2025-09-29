1d873d035f14f2e96e3e361caa9f3a292020681d
Karim Mhamdi <karimmhamdi@usf.edu>
2025-09-28 23:24:46 -0400

    Steps 3 & 4 hopefully done

--- cc_5.js
+++ cc_5.js
@@ -3,8 +3,28 @@
 // Step 2
 const employees = [
   { name: "John Smith", hourlyRate: 29.99, hoursWorked: 8 },
-  { name: "Robert Dwoney Jr.", hourlyRate: 99.50,         hoursWorked: 10  },
+  { name: "Robert Downey Jr.", hourlyRate: 99.50,         hoursWorked: 10  },
   { name: "Amuro Ray", hourlyRate: 49.99, hoursWorked: 6  },
 ];
 
 // Step 3
+function calculateBasePay(rate, hours) {
+  let regularHours;
+  if (hours > 40) {
+    regularHours = 40;
+  } else {
+    regularHours = hours;
+  }
+  return rate * regularHours;
+}
+
+// Step 4
+function calculateOvertimePay(rate, hours) {
+  let overtimeHours;
+  if (hours > 40) {
+    overtimeHours = hours - 40;
+  } else {
+    overtimeHours = 0;
+  }
+  return overtimeHours * rate * 1.5;
+}
